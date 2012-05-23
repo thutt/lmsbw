03073ae20de58a0d3c225f5e9db85f3bbc24181b
thutt <thutt@harp-project.com>
2012-05-22 21:49:14 -0700

    Refactored source report to be based on the type of the module.

--- wrapper/engine/generate-report.mk
+++ wrapper/engine/generate-report.mk
@@ -1,20 +1,65 @@
+# Copyright (c) 2012 Taylor Hutt, Logic Magicians Software
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# generate_component_report_source <component>
+#
+#   Generates a report for a 'source' module.
+#
+define generate_component_report_source
+__$(strip $(1)).report.$(call get,LMSBW_$(strip $(1)),kind):
+	@$(ECHO) "$(1)|Module      : $(call get,LMSBW_$(strip $(1)),module)";
+	@$(ECHO) "$(1)|  Component : $(call get,LMSBW_$(strip $(1)),component)";
+	@$(ECHO) "$(1)|  Kind      : $(call get,LMSBW_$(strip $(1)),kind)";
+	@$(ECHO) "$(1)|  Reason    : $(call get,LMSBW_$(strip $(1)),reason)";
+	@$(ECHO) "$(1)|  Source    : $(call get,LMSBW_$(strip $(1)),source-directory)";
+	@$(ECHO) "$(1)|Build Root  : $(call get,LMSBW_$(strip $(1)),build-root-directory)";
+	@$(ECHO) "$(1)|  Build     : $(call get,LMSBW_$(strip $(1)),build-directory)";
+	@$(ECHO) "$(1)|  DESTDIR   : $(call get,LMSBW_$(strip $(1)),destdir-directory)";
+	@$(ECHO) "$(1)|sysroot     : $(call get,LMSBW_$(strip $(1)),sysroot-directory)";
+	@$(ECHO) "$(1)|Prerequisite: $(call get,LMSBW_$(strip $(1)),prerequisite)";
+endef
+
+# generate_component_report_download <component>
+#
+#   Generates a report for a 'download' module.
+#
+define generate_component_report_download
+$(error $(0): function is not implemented.)
+endef
+
+
 # generate_component_report <component>
+#
+#   Generates a report for any type of module.
 #
 define generate_component_report
 component-report:: $(strip $(1)).report
+$(call assert,											\
+	$(call or,										\
+		$(call seq,$(call get,LMSBW_$(strip $(1)),kind),source),			\
+		$(call seq,$(call get,LMSBW_$(strip $(1)),kind),download)),			\
+	Module kind '$(call get,LMSBW_$(strip $(1)),kind)' is not 'source' nor 'download')
 
-.PHONY:	$(strip $(1)).report
 
-$(strip $(1)).report:
-	@$(ECHO) "$(1)|Module      : $(call get,LMSBW_$(strip $(1)),module)";
-	@$(ECHO) "$(1)|Component   : $(call get,LMSBW_$(strip $(1)),component)";
-	@$(ECHO) "$(1)|Kind        : $(call get,LMSBW_$(strip $(1)),kind)";
-	@$(ECHO) "$(1)|Reason      : $(call get,LMSBW_$(strip $(1)),reason)";
-	@$(ECHO) "$(1)|Source      : $(call get,LMSBW_$(strip $(1)),source-directory)";
-	@$(ECHO) "$(1)|Build       : $(call get,LMSBW_$(strip $(1)),build-directory)";
-	@$(ECHO) "$(1)|DESTDIR     : $(call get,LMSBW_$(strip $(1)),destdir-directory)";
-	@$(ECHO) "$(1)|sysroot     : $(call get,LMSBW_$(strip $(1)),sysroot-directory)";
-	@$(ECHO) "$(1)|Prerequisite: $(call get,LMSBW_$(strip $(1)),prerequisite)";
+.PHONY:	$(strip $(1)).report __$(strip $(1)).report.$(call get,LMSBW_$(strip $(1)),kind)
+
+$(call generate_component_report_$(call get,LMSBW_$(strip $(1)),kind),$(1))
+
+
+$(strip $(1)).report: __$(strip $(1)).report.$(call get,LMSBW_$(strip $(1)),kind)
 
 endef
 
