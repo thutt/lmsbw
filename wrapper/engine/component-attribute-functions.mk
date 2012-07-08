1391e68f5b46b27921e71e2d99e297c9a4c18a02
thutt <thutt@harp-project.com>
2012-07-08 10:55:40 -0700

    Renamed some component declaration functions to be 'attributes'.
    This change alters the semantics of the following to be attributes of a component
    
       o no parallel build
       o build target
       o install target
       o api
    
    The decision to do this is based on making it easier to search
    configuration files for apsects of components which are not specific
    to their declaration to LMSBW proper.  By annotating these items with
    an 'attribute', it become easier to search for these items.

---
+++ wrapper/engine/component-attribute-functions.mk
@@ -0,0 +1,80 @@
+# Copyright (c) 2012 Taylor Hutt, Logic Magicians Software
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# component_attribute_no_parallel_build <component>
+#
+#   Ensures that the individual component bearing this attribute will
+#   not be built in parallel.
+#
+#   This attribute, if present, is assigned to LMSBW_C_NO_PARALLEL
+#   when invoking 'component.makefile'.  If not present,
+#   LMSBW_C_NO_PARALLEL is defined, but will have no value.
+#
+define component_attribute_no_parallel_build
+$(call lmsbw_assert_known_component,$(1))
+lmsbw_dcnpb:=$(call lmsbw_scf,$(1),no-parallel,-j 1)
+endef
+
+
+# component_attribute_build_target <component> <list of targets>
+#
+#  Sets the targets that will be passed to the component's Makefile as
+#  the preliminary targets for the 'bulid' phase.
+#
+#  If not set, LMSBW will not use a specific set of targets for the
+#  'build' phase, and will end up using the default (or first) rule in
+#  the component's Makefile.
+#
+define component_attribute_build_target
+$(call lmsbw_assert_known_component,$(1))
+lmsbw_dcbt:=$(call lmsbw_scf,$(1),build-target,$(2))
+endef
+
+
+# component_attribute_install_target <component> <list of targets>
+#
+#  Sets the targets that will be passed to the components's Makefile
+#  as the preliminary targets for the 'install' phase.
+#
+#  If not set, LMSBW will set it to 'install'.
+#
+define component_attribute_install_target
+$(call lmsbw_assert_known_component,$(1))
+lmsbw_dcbt:=$(call lmsbw_scf,$(1),install-target,$(2))
+endef
+
+
+# component_attribute_api <component>, <list of directories>
+#
+#  This function declares a list of directories which contain the
+#  public API of the component.  The directories can contain the
+#  source API (header files) and/or the binary API (static libs).
+#
+#  This is be used to cause dependent components to be rebuilt *only*
+#  when the API has been changed.
+#
+#  The files installed in $(DESTDIR) are used when checking if the API
+#  has changed.
+#
+#  If you are using dynamic libraries, the directories in which they
+#  are installed should not be included in the API directories; this
+#  is because internal changes to dynamic libraries do not affect
+#  components which are simply linked to them.
+#
+define component_attribute_api
+$(call lmsbw_assert_known_component,$(1))
+lmsbw_dca:=$(call lmsbw_scf,$(1),api,$(strip $(2)))
+endef
+
