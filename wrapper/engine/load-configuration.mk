2e0bb174760ab48242cc01d8cd493ce5858c5abd
thutt <thutt@harp-project.com>
2012-08-24 15:45:59 -0700

    Put all function definitions at the top

--- wrapper/engine/load-configuration.mk
+++ wrapper/engine/load-configuration.mk
@@ -34,49 +34,6 @@ $(if $(call seq,$(call __gcv,component-build-support),),	\
 $(foreach f,$(addsuffix -component.mk,$(addprefix $(LMSBW_DIR)/wrapper/engine/build-support-,$(call __gcv,component-build-support))),$(call assert_exists,$(f)) $(eval include $(f)))
 endef
 
-# Include configuration information for wrapped build system:
-#
-#  o Top-level targets
-#  o Function to load configuration files
-#
-#  The value must be the absolute pathname of the configuration file.
-#  This is enforced by the lmsbw script.
-#
-ifndef LMSBW_CONFIGURATION_FILE
-$(call lmsbw_error,E1006,LMSBW_CONFIGURATION_FILE is not defined; no wrapped build system)
-endif
-
-$(call assert_exists,$(LMSBW_CONFIGURATION_FILE))
-
-include $(LMSBW_CONFIGURATION_FILE)
-
-# Validate the declared 'load-configuration-function'
-#
-#   o Associative array element set
-#   o The value references a symbol which was defined in a Makefile
-#
-# The referenced function must load all configuration files.
-#
-# The name of each loaded component must be placed onto the
-# 'LMSBW_components' list using GMSL functions.
-#
-# The 'load-configuration-function' can use the
-# 'LMSBW_CONFIGURATION_FILE' variable to determine where it resides on
-# disk.  This facilitates loading the definition files relative to the
-# configuration file.
-#
-# If the function returns (rather than exiting with an error), all
-# 'component' declarations have been processed.  The build wrapper
-# will proceed to validate them.
-#
-$(call lmsbw_check_load_configuration_function)
-$(call lmsbw_load_component_support)
-
-# Load component configuration files.
-#
-$(eval $(call $(call __gcv,load-configuration-function),$(LMSBW_CONFIGURATION_FILE)))
-$(call preqrequisite_test)
-
 # check_components_form_dag_work <main component>,
 #                                <checking component>,
 #                                <prerequisites of checking component>
@@ -125,6 +82,49 @@ $(call check_components_form_dag_work,$(1),$(2),$(3))
 $(eval lmsbw_dag_list:=)
 $(eval lmsbw_dag_set:=$(empty_set))
 endef
+
+# Include configuration information for wrapped build system:
+#
+#  o Top-level targets
+#  o Function to load configuration files
+#
+#  The value must be the absolute pathname of the configuration file.
+#  This is enforced by the lmsbw script.
+#
+ifndef LMSBW_CONFIGURATION_FILE
+$(call lmsbw_error,E1006,LMSBW_CONFIGURATION_FILE is not defined; no wrapped build system)
+endif
+
+$(call assert_exists,$(LMSBW_CONFIGURATION_FILE))
+
+include $(LMSBW_CONFIGURATION_FILE)
+
+# Validate the declared 'load-configuration-function'
+#
+#   o Associative array element set
+#   o The value references a symbol which was defined in a Makefile
+#
+# The referenced function must load all configuration files.
+#
+# The name of each loaded component must be placed onto the
+# 'LMSBW_components' list using GMSL functions.
+#
+# The 'load-configuration-function' can use the
+# 'LMSBW_CONFIGURATION_FILE' variable to determine where it resides on
+# disk.  This facilitates loading the definition files relative to the
+# configuration file.
+#
+# If the function returns (rather than exiting with an error), all
+# 'component' declarations have been processed.  The build wrapper
+# will proceed to validate them.
+#
+$(call lmsbw_check_load_configuration_function)
+$(call lmsbw_load_component_support)
+
+# Load component configuration files.
+#
+$(eval $(call $(call __gcv,load-configuration-function),$(LMSBW_CONFIGURATION_FILE)))
+$(call preqrequisite_test)
 
 $(foreach c,$(LMSBW_components),				\
 	$(call lmsbw_assert_not_self_prerequisite,$(c))		\
